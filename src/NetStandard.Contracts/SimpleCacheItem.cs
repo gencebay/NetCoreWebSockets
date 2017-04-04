dd761dde84b027efe4df92c3aa68cf2a55e1d58b
Gencebay Demir <gencebay.demir@gmail.com>
2017-04-04 09:52:54 +0300

    Init commit

---
+++ src/NetStandard.Contracts/SimpleCacheItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NetStandard.Contracts
+{
+    public class SimpleCacheItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
